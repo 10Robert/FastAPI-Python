0fcc3cffac4a1a2c71112d66ec6970358bfb1cca
10Robert <robertlucasmtz124@gmail.com>
2024-11-08 06:43:49 -0300

    Hash User's password

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,10 +1,13 @@
+import bcrypt
 from fastapi import APIRouter
 from pydantic import BaseModel
 from models import Users
 from starlette import status
+from passlib.context import CryptContext
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -25,7 +28,7 @@ async def create_user(create_user_request: CreateUserRequest):
         first_name = create_user_request.first_name,
         last_name = create_user_request.last_name,
         role = create_user_request.role,
-        hashed_password = create_user_request.password,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
         is_active = True
     )
 
