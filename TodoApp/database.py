c5a0a3a647c57e8716b45819f5725e8c5aaf5093
10Robert <robertlucasmtz124@gmail.com>
2024-11-08 06:05:37 -0300

    Create a users table and a relationship with a pendencias table.

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosAPP.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,7 +1,20 @@
+import email
 from pydoc import describe
 from turtle import title
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+class Users(Base):
+    __tablename__: str = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column()
+    is_active = Column(Boolean, default=False)
+    role = Column(String)
+
 
 class Todos(Base):
     __tablename__ = "todos"
@@ -11,3 +24,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id")) 
